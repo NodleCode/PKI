de661bcfa4d71f6c2ed2420d674e1ebbd34a1c70
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-03-25 17:35:55 -0700

    implement 'new'

--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
+const { Keyring } = require('@polkadot/api');
+const { u8aToHex } = require('@polkadot/util');
+const { randomAsU8a } = require('@polkadot/util-crypto');
+
 require('yargs')
 	.usage('Usage: $0 [--seed <seed>] <command> [options]')
-	.command('new', 'Generate new signing keys that can be registered by an authority')
 	.command(
-		'inspect <signingPublicKey>',
+		'new',
+		'Generate new signing keys that can be registered by an authority',
+		(b) => b,
+		(argv) => {
+			const keyring = new Keyring({ type: 'ed25519' });
+	        const seed = randomAsU8a(32);
+    	    const newKey = keyring.addFromSeed(seed);
+
+        	console.log(`Address ........ : ${newKey.address}`);
+        	console.log(`Public key ..... : ${u8aToHex(newKey.publicKey)}`);
+        	console.log(`Seed ........... : ${u8aToHex(seed)}`);
+		},
+	)
+	.command(
+		'inspect <signingAddress>',
 		'Display the status of a slot',
-		(b) => b.positional('signingPublicKey', {
+		(b) => b.positional('signingAddress', {
 			describe: 'the registered on-chain public signing key',
 			type: 'string'
 		}),
@@ -31,39 +48,39 @@ require('yargs')
 		console.log,
 	)
 	.command(
-		'book <signingPublicKey>',
+		'book <signingAddress>',
 		'Book a slot and link it to a given signing key',
-		(b) => b.positional('signingPublicKey', {
+		(b) => b.positional('signingAddress', {
 			describe: 'the to-be-registered on-chain public signing key',
 			type: 'string'
 		}),
 		console.log,
 	)
 	.command(
-		'renew <signingPublicKey>',
+		'renew <signingAddress>',
 		'Renew a given slot',
-		(b) => b.positional('signingPublicKey', {
+		(b) => b.positional('signingAddress', {
 			describe: 'the registered on-chain public signing key',
 			type: 'string'
 		}),
 		console.log,
 	)
 	.command(
-		'revoke <signingPublicKey>',
+		'revoke <signingAddress>',
 		'Revoke a slot and its associated certificates all together',
-		(b) => b.positional('signingPublicKey', {
+		(b) => b.positional('signingAddress', {
 			describe: 'the registered on-chain public signing key',
 			type: 'string'
 		}),
 		console.log,
 	)
 	.command(
-		'revoke_cert <signingPublicKey> <devicePublicKey>',
+		'revoke_cert <signingAddress> <deviceAddress>',
 		'Revoke a certificate',
-		(b) => b.positional('signingPublicKey', {
+		(b) => b.positional('signingAddress', {
 			describe: 'the registered on-chain public signing key',
 			type: 'string'
-		}).positional('devicePublicKey', {
+		}).positional('deviceAddress', {
 			describe: 'the public key of the device',
 			type: 'string'
 		}),
