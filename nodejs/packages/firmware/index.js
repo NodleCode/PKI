ce5adbf1cd95da15bf1db21e1ea2edefc7c4b9d3
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-06-24 11:55:31 -0700

    implement mode state machine

--- nodejs/packages/firmware/index.js
+++ nodejs/packages/firmware/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-//const { Certificate, Runtime } = require('pki');
 const Keystore = require('./keystore');
 const argv = require('yargs')
     .usage('Usage: $0 [--keystore <keystore_path>]')
@@ -9,12 +8,37 @@ const argv = require('yargs')
     .epilog('copyright Nodle 2020')
     .argv;
 
+const enterFactoryMode = () => {
+    console.log('Factory mode');
+}
+
+const enterOperatingMode = () => {
+    console.log('Operating mode');
+}
+
 const main = async () => {
     if (argv.keystore === undefined) {
         argv.keystore = '~/.nodle_pki_keystore.json';
     }
 
+    // First we load the local devices keys, if none are present
+    // we generate them. Basically we generate the keys locally
+    // on the first boot.
     const keystore = new Keystore(argv.keystore);
+
+    // If no certificates are present we enter the 'Factory Mode',
+    // basically we wait for an operator to burn our certificate
+    // into the device.
+    // We apply the trust on first use principle, which means that
+    // whoever burn the certificate first is trusted. In a real
+    // world production scenario the keys would be managed through
+    // a secure element and the certificate provisioned at the
+    // factory in the assembly lines.
+    if (!keystore.hasCertificate()) {
+        enterFactoryMode();
+    } else {
+        enterOperatingMode();
+    }
 }
 
 main().then(() => { process.exit(0) });
--- nodejs/packages/firmware/keystore.js
+++ nodejs/packages/firmware/keystore.js
@@ -1,3 +1,5 @@
+const { Certificate, Runtime } = require('pki');
+
 const { Keyring } = require('@polkadot/api');
 const { randomAsU8a } = require('@polkadot/util-crypto');
 const { hexToU8a, u8aToHex } = require('@polkadot/util');
@@ -8,28 +10,34 @@ class Keystore {
     seed = null;
     keyring = null;
     account = null;
+    certificate = undefined;
 
     constructor(path) {
         const maybeExpanded = expandHomeDir(path);
+        let keystore = null;
         if (fs.existsSync(maybeExpanded)) {
-            this.seed = this.loadSeed(maybeExpanded);
+            keystore = this.loadKeystore(maybeExpanded);
         } else {
-            this.seed = this.generateAndSave(maybeExpanded);
+            keystore = this.generateAndSaveKeystore(maybeExpanded);
         }
 
-        // Load seed
+        // Load seed and certificate
         this.keyring = new Keyring({ type: 'ed25519' });
-        this.account = this.keyring.addFromSeed(this.seed);
+        this.account = this.keyring.addFromSeed(keystore.seed);
+        this.certificate = keystore.certificate;
     }
 
-    loadSeed(path) {
+    loadKeystore(path) {
         const rawdata = fs.readFileSync(path);
         const parsed = JSON.parse(rawdata);
 
-        return hexToU8a(parsed.seed);
+        return {
+            seed: hexToU8a(parsed.seed),
+            certificate: parsed.certificate,
+        };
     }
 
-    generateAndSave(path) {
+    generateAndSaveKeystore(path) {
         const seed = randomAsU8a(32);
 
         const keystore = {
@@ -38,7 +46,14 @@ class Keystore {
         const data = JSON.stringify(keystore);
         fs.writeFileSync(path, data);
 
-        return seed;
+        return {
+            seed: seed,
+            certificate: undefined,
+        };
+    }
+
+    hasCertificate() {
+        return this.certificate !== undefined;
     }
 }
 
