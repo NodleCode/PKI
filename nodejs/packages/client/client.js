f2971e5396bd2053f2c2b3efe52f9cc441ab1160
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-06-29 11:49:32 -0700

    fetch identity and create certificate

--- nodejs/packages/client/client.js
+++ nodejs/packages/client/client.js
@@ -1,10 +1,29 @@
+const axios = require('axios');
+const urljoin = require('url-join');
+
+const { Certificate } = require('pki');
+
+const PATH_IDENTITY = '/identity';
+
 class FirmwareClient {
+    url = '';
+
     constructor(url) {
         this.url = url;
     }
 
-    async burn() {
-        console.log('TODO');
+    async burn(pair, expiry) {
+        const identity = await axios.get(urljoin(this.url, PATH_IDENTITY));
+        const deviceAddress = identity.data.address;
+
+        if (identity.data.hasCertificate) {
+            throw new Error('Firmware no longer in factory mode');
+        }
+
+        const certificate = new Certificate({ device: deviceAddress, pair: pair, expiry: expiry });
+        const encoded = certificate.signAndEncode();
+
+        console.log(encoded);
     }
 }
 
