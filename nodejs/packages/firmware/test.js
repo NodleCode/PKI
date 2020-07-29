f3d21b08537677c2d1b360853987bce2fc468fff
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-07-29 11:45:17 -0700

    implement keystore tests

--- nodejs/packages/firmware/test.js
+++ nodejs/packages/firmware/test.js
@@ -1,3 +1,24 @@
+const Keystore = require("./keystore")
+const { Certificate } = require('pki');
+const { Keyring } = require('@polkadot/api');
+const { randomAsU8a } = require('@polkadot/util-crypto');
+const { u8aToHex } = require('@polkadot/util');
+const moment = require('moment');
+const { expect } = require("chai");
+
+const mockCert = (signer, device) => {
+    const keyring = new Keyring({ type: 'ed25519' });
+    const pair = keyring.addFromUri(signer);
+
+    const paramsCert = {
+        device: device,
+        pair: pair,
+        expiry: moment().add(1, 'month')
+    };
+    const cert = new Certificate(paramsCert);
+    return cert.signAndEncode();
+}
+
 describe('Firmware', () => {
     context('Helpers', () => {
         context('validateAndStoreNewCertificate', () => {
@@ -35,13 +56,51 @@ describe('Firmware', () => {
 })
 
 describe('Keystore', () => {
-    context('Certificates', () => {
-        it('load and save correctly with one certificate')
+    const randomKeystoreName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    const keystorePath = `/tmp/${randomKeystoreName}.json`;
 
-        it('load and save correctly with many certificates')
+    const keystore = new Keystore(keystorePath);
+
+    it('generates an empty on disk file', () => {
+        expect(require('fs').existsSync(keystorePath)).to.be.true;
+    })
+
+    context('Certificates', () => {
+        it('starts empty', () => {
+            expect(keystore.hasCertificate()).to.be.false;
+        })
+
+        it('load and save correctly with one certificate', () => {
+            const cert = mockCert('0xa35be3bd72de762ce02b113bbeef4c553d3ded70f014e364ff38ebeb1f3130c1', keystore.account.address);
+            keystore.saveCertificate(cert);
+
+            expect(keystore.hasCertificate()).to.be.true;
+
+            const loaded = new Keystore(keystorePath);
+            expect(loaded.hasCertificate()).to.be.true;
+            expect(loaded.certificates.length).to.be.equal(1);
+            expect(loaded.certificates[0]).to.be.equal(cert);
+        })
+
+        it('load and save correctly with many certificates', () => {
+            const cert = mockCert('0xbd1992343b5ea318149a0646aef6162306f45ec3346abfdea6d5ac22788c1520', keystore.account.address);
+            keystore.saveCertificate(cert);
+
+            expect(keystore.hasCertificate()).to.be.true;
+
+            const loaded = new Keystore(keystorePath);
+            expect(loaded.hasCertificate()).to.be.true;
+            expect(loaded.certificates.length).to.be.equal(2);
+            expect(loaded.certificates).to.contain(cert);
+        })
     })
 
     context('Challenges', () => {
-        it('succesfully solve challenges')
+        it('succesfully solve challenges', () => {
+            const challenge = randomAsU8a(32);
+            const signed = keystore.signChallenge(u8aToHex(challenge));
+
+            expect(keystore.account.verify(challenge, signed)).to.be.true;
+        })
     })
 })
